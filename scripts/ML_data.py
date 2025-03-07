30a2362c6f6173ebdb0870de2fbae88daab6b4dd
Jiefei-Wang <szwjf08@gmail.com>
2025-03-07 10:20:13 -0600

    rename files

--- scripts/ML_data.py
+++ scripts/ML_data.py
@@ -1,42 +1,102 @@
-## This file is used to generate the data for ML model
-##
-## Idea: map the source data with either
-## 1. name only
-## 2. explanation only
-## 3. name + explanation
-## to standard concept: Name + explanation
-
+# This file is used to generate the mapping from 
+# standard to non-standard concepts
+#
+# For each standard concept, create columns:
+# - nonstd_ids: list of non-standard concept ids
+# - nonstd_names: list of non-standard concept names (size = nonstd_id)
+# - descriptions: list of concept descriptions (from UMLS)
+# - synonym_names: list of concept synonyms (if no synonym, then empty list)
+# If a value in a column is empty, it must be None
+# 
+# Store the result in data/ML/conceptML.feather
+import importlib
+import modules
 from tqdm import tqdm
 import pandas as pd
 from modules.ML_extract_name import extract_nonstd_names, extract_synonym, extract_umls_description
 from modules.UMLS_file import read_mrconso, read_mrdef
 
+# Reload the custom module to ensure latest version of functions are used
+importlib.reload(modules.ML_extract_name)
 
 
-
-## Read OMOP datasets
+# Load OMOP CDM concept tables from feather files
 concept = pd.read_feather('data/omop_feather/concept.feather')
 concept_relationship = pd.read_feather('data/omop_feather/concept_relationship.feather')
 concept_synonym = pd.read_feather("data/omop_feather/concept_synonym.feather")
-concept_ancestor = pd.read_feather("data/omop_feather/concept_ancestor.feather")
 
 
-## Read UMLS datasets
-## UMLS data contains descriptions of the concepts
+# Load and process UMLS reference files
+# UMLS data contains descriptions of the concepts
 mrconso_path = "data/UMLS_raw/MRCONSO.RRF"
 umls_def_path = "data/UMLS_raw/MRDEF.RRF"
-mrconso_df = read_mrconso(mrconso_path)
-mrdef_df = read_mrdef(umls_def_path)
+mrconso_df = read_mrconso(mrconso_path) # Reads UMLS concept names and relationships
+mrdef_df = read_mrdef(umls_def_path) # Reads UMLS concept definitions
 
 
-## Obtain concept information
+# Extract standard concept names and descriptions
 std_concept = concept[concept['standard_concept'] == 'S']
-nonstd_names = extract_nonstd_names(concept, concept_relationship)
+nonstd_concept = concept[concept['standard_concept'] != 'S']
+
+
+std_conditions = std_concept[std_concept.domain_id == 'Condition']
+std_conditions.groupby('vocabulary_id')['concept_id'].count()
+"""
+HCPCS                    1
+ICDO3                56858
+Nebraska Lexicon      1274
+OMOP Extension         341
+OPCS4                    1
+SNOMED               98720
+SNOMED Veterinary     3093
+"""
+
+nonstd_conditions = nonstd_concept[nonstd_concept.domain_id == 'Condition']
+nonstd_conditions.groupby('vocabulary_id')['concept_id'].count()
+"""
+CDISC                   455
+CIEL                  38818
+CIM10                 13885
+CO-CONNECT               16
+Cohort                   66
+HemOnc                  260
+ICD10                 14113
+ICD10CM               88510
+ICD10CN               30588
+ICD10GM               15952
+ICD9CM                14929
+ICDO3                  5677
+KCD7                  19705
+MeSH                  12343
+Nebraska Lexicon     150062
+OMOP Extension            8
+OPCS4                     5
+OXMIS                  5704
+OncoTree                885
+PPI                      74
+Read                  47836
+SMQ                     324
+SNOMED                58172
+SNOMED Veterinary       144
+"""
+
+# nonstd_concept[nonstd_concept['vocabulary_id'] == 'CIEL'].concept_name
+
+
+# Extract a mapping of standard concepts to non-standard names
+nonstd_names = extract_nonstd_names(concept, concept_relationship) 
+
+# Get concept synonyms
 synonum_names = extract_synonym(concept, concept_synonym)
+
+# Get UMLS definitions
 umls_names = extract_umls_description(concept, mrconso_df, mrdef_df)
 
-
+# Define essential columns to keep from standard concepts
 column_keep = ['concept_id', 'concept_name', 'domain_id', 'vocabulary_id', 'concept_code']
+
+# Merge all concept information
+# Performs left joins to preserve all standard concepts while adding additional naming information
 conceptML = pd.merge(
     std_concept[column_keep],
     nonstd_names,
@@ -50,13 +110,18 @@ conceptML = pd.merge(
     umls_names,
     on = 'concept_id',
     how = 'left'
+).rename(columns={
+    'umls_desc': 'descriptions',
+    'concept_synonym_name': 'synonym_name'}
 )
 
-## combine all names into one
-columns_combine = ['nonstd_name', 'concept_synonym_name', 'umls_desc']
-conceptML1 = conceptML.copy()
-conceptML1['text'] = conceptML1[columns_combine].apply(lambda x: [i for k in x if isinstance(k, list) for i in k], axis=1)
 
-conceptML1.to_feather('data/ML/conceptML1.feather')
+## combine all names into one
+# columns_combine = ['nonstd_name', 'concept_synonym_name', 'umls_desc']
+# conceptML1 = conceptML.copy()  
+# conceptML1['text'] = conceptML1[columns_combine].apply(lambda x: [i for k in x if isinstance(k, list) for i in k], axis=1)
+
+# Save the final concept mapping table
+conceptML.to_feather('data/ML/conceptML1.feather')
 
 
--- scripts/ML_train.py
+++ scripts/ML_train.py
@@ -1,40 +1,122 @@
-from sentence_transformers import SentenceTransformer, SentenceTransformerTrainer, losses
-from datasets import Dataset
-dataset2 = Dataset.load_from_disk('data/train_dataset')
+import os
+os.environ['FOR_DISABLE_CONSOLE_CTRL_HANDLER'] = '1'
 
-## trainint and testing split
-dt = dataset2.train_test_split(test_size=0.001, seed=42)
-train_dataset = dt['train']
-test_dataset = dt['test']
-
-
-model = SentenceTransformer('models/all-MiniLM-L6-v2')
-train_loss = losses.ContrastiveLoss(model=model)
-
-
+import torch
+import pandas as pd
+import numpy as np
+# from transformers import AutoTokenizer, AutoModel
+from sentence_transformers import SentenceTransformer, SentenceTransformerTrainer, losses, models
 from sentence_transformers.training_args import SentenceTransformerTrainingArguments
+from datasets import Dataset, DatasetDict, concatenate_datasets
+
+## make sure cuda is available
+torch.cuda.is_available()
+
+
+model_name = 'models/all-MiniLM-L6-v2'
+# model_name = 'models/base_exclude_CIEL/checkpoint-65000'
+output_dir = "models/base_exclude_CIEL2"
+
+ds_names = ['matching', 'relation']
+
+conceptEX = pd.read_feather('data/omop_feather/conceptEX.feather')
+matching_all = pd.read_feather('data/ML/conceptML_matching.feather')
+relation_all = pd.read_feather('data/ML/conceptML_relation.feather')
+matching_validation = pd.read_feather('data/ML/conceptML_matching_validation.feather')
+
+matching_validation_sub = matching_validation.sample(frac=0.1, random_state=42)
+dataset_matching = Dataset.from_pandas(matching_all)
+dataset_relation = Dataset.from_pandas(relation_all)
+
+print(dataset_matching)
+print(dataset_relation)
+
+## empty dataset
+relation_validation = Dataset.from_dict(dataset_relation[:0])
+
+
+## train test split
+# dataset_matching_split = dataset_matching.train_test_split(test_size=0.0005, seed=42)
+# dataset_relation_split = dataset_relation.train_test_split(test_size=0.0001, seed=42)
+
+# dataset_matching_train = dataset_matching_split['train']
+# dataset_matching_test = dataset_matching_split['test']
+# dataset_relation_train = dataset_relation_split['train']
+# dataset_relation_test = dataset_relation_split['test']
+
+
+train_dataset = {
+    'matching': dataset_matching,
+    'relation': dataset_relation
+}
+train_dataset = {k: train_dataset[k] for k in ds_names}
+test_dataset = {
+    'matching': matching_validation_sub,
+    'relation': relation_validation
+}
+test_dataset = {k: test_dataset[k] for k in ds_names}
+
+print(train_dataset)
+print(test_dataset)
+
+
+
+model = SentenceTransformer(model_name)
+
+## add special tokens
+transformer = model[0]  
+tokenizer = transformer.tokenizer
+auto_model = transformer.auto_model
+
+# Now you can add special tokens to the tokenizer
+special_tokens_dict = {
+    "additional_special_tokens": ["[MATCHING]", "[RELATION]"]
+}
+num_added_tokens = tokenizer.add_special_tokens(special_tokens_dict)
+
+# Resize the model embeddings if we added tokens
+if num_added_tokens > 0:
+    auto_model.resize_token_embeddings(len(tokenizer))
+    
+
+from sentence_transformers.evaluation import BinaryClassificationEvaluator
+
+matching_validation_sub.reset_format()
+# Initialize the evaluator
+dev_evaluator1 = BinaryClassificationEvaluator(
+    sentences1=matching_validation_sub["sentence1"],
+    sentences2=matching_validation_sub["sentence2"],
+    labels=matching_validation_sub["label"],
+    name="evaluation1",
+)
+dev_evaluator1(model)
+
+train_loss = {
+    'matching' : losses.ContrastiveLoss(model=model),
+    'relation' : losses.ContrastiveLoss(model=model)
+}
+train_loss = {k: train_loss[k] for k in ds_names}
 
 args = SentenceTransformerTrainingArguments(
     # Required parameter:
-    output_dir="models/fine-tuned",
+    output_dir=output_dir,
     # Optional training parameters:
-    num_train_epochs=10,
-    per_device_train_batch_size=128,
-    per_device_eval_batch_size=128,
+    num_train_epochs=5,
+    per_device_train_batch_size=256,
+    per_device_eval_batch_size=256,
     warmup_ratio=0.1,
     fp16=True,  # Set to False if your GPU can't handle FP16
     bf16=False,  # Set to True if your GPU supports BF16
     # batch_sampler=BatchSamplers.NO_DUPLICATES,  # Losses using "in-batch negatives" benefit from no duplicates
     # Optional tracking/debugging parameters:
     eval_strategy="steps",
-    eval_steps=100,
+    eval_steps=1000,
     save_strategy="steps",
-    save_steps=1000,
+    save_steps=10000,
     save_total_limit=2,
-    logging_steps=100,
-    run_name="mpnet-base-all-nli-triplet",  # Used in W&B if `wandb` is installed
+    logging_steps=500,
+    run_name=output_dir,  # Used in W&B if `wandb` is installed
 )
-
 
 trainer = SentenceTransformerTrainer(
     model=model,
@@ -42,5 +124,11 @@ trainer = SentenceTransformerTrainer(
     eval_dataset=test_dataset,
     loss=train_loss,
     args=args,
+    evaluator=dev_evaluator1,
 )
+
+
 trainer.train()
+
+
+model.save(f"{output_dir}/final")
